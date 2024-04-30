343ec6b5e5f8cc7792a46982ddccb0b1712b7576
YACHAE <104hsh76@gmail.com>
2024-04-30 21:50:45 +0900

    submit버튼 삭제, 입력시 오류 상태알림 세분화
    비어있는경우, 중복된단어, 나와있는 단어와 같은 단어일떄)

--- EngAttack/ContentViewViewModel.swift
+++ EngAttack/ContentViewViewModel.swift
@@ -8,133 +8,149 @@
 import Foundation
 
 class ContentViewViewModel: ObservableObject {
-let starterWords = ["apple", "banana", "cherry", "date", "elderberry"]
-@Published var currentWord = ""
-@Published var userInput = ""
-@Published var score = 0
-@Published var alertTitle = ""
-@Published var showAlert = false
-@Published var usedWords = Set<String>()
-@Published var bookmarkedWords = Set<String>()
-@Published var isDarkMode = false
-@Published var timeRemaining = 5.0
-@Published var timerIsActive = false
-
-var gameTimer: Timer?
-let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-private var wordManager = WordManager()
-
-init() {
-pickRandomWord()
-}
-
-func pickRandomWord() {
-self.currentWord = wordManager.pickRandomWord()
-}
-
-func submitButton() {
-let word = userInput.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-let word2 = currentWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-// 입력한 단어가 비어있거나, 이전에 사용된 단어인 경우, 혹은 현재 단어와 동일한 경우 먼저 검사
-guard word.count > 0, !usedWords.contains(word), word != currentWord else {
-self.alertTitle = "잘못된 입력"
-self.showAlert = true
-self.userInput = ""
-return
-}
-
-// 단어의 끝 알파벳과 입력한 단어의 첫 알파벳이 일치하는지 검사
-guard let lastChar = word2.last, let firstChar = word.first, lastChar == firstChar else {
-  self.alertTitle = "단어의 첫 글자가 이전 단어의 마지막 글자와 일치하지 않습니다."
-  self.showAlert = true
-  self.userInput = ""
-  return
-}
-
-// 유효성 검사
-fetchWordInfo(word: word) { isValid in
-  if isValid {
-	DispatchQueue.main.async {
-	  self.usedWords.insert(word)
-	  self.currentWord = word
-	  self.score += 1
-	  self.userInput = ""
-
-	}
-  } else {
-	DispatchQueue.main.async {
-	  self.alertTitle = "잘못된 단어입니다"
+  let starterWords = ["apple", "banana", "cherry", "date", "elderberry"]
+  @Published var currentWord = ""
+  @Published var userInput = ""
+  @Published var score = 0
+  @Published var alertTitle = ""
+  @Published var showAlert = false
+  @Published var usedWords = Set<String>()
+  @Published var bookmarkedWords = Set<String>()
+  @Published var isDarkMode = false
+  @Published var timeRemaining = 10.0
+  @Published var timerIsActive = false
+  
+  
+  var gameTimer: Timer?
+  let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+  private var wordManager = WordManager()
+  
+  init() {
+	pickRandomWord()
+  }
+  
+  func pickRandomWord() {
+	self.currentWord = wordManager.pickRandomWord()
+  }
+  
+  func submitButton() {
+	let word = userInput.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+	let word2 = currentWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+	// 입력한 단어가 비어있는 경우
+	guard word.count > 0 else {
+	  self.alertTitle = "입력된 단어가 없습니다"
 	  self.showAlert = true
+	  self.userInput = ""
+	  return
 	}
+	
+	// 입력한 단어가 이전에 사용된 단어인 경우,
+	guard !usedWords.contains(word) else {
+	  self.alertTitle = "이미 사용된 단어입니다"
+	  self.showAlert = true
+	  self.userInput = ""
+	  return
+	}
+	
+	// 입력한 단어가 현재의 단어와 동일한 경우
+	guard word != currentWord else {
+	  self.alertTitle = "현재 단어와 동일합니다"
+	  self.showAlert = true
+	  self.userInput = ""
+	  return
+	}
+	
+	// 단어의 끝 알파벳과 입력한 단어의 첫 알파벳이 일치하는지 검사
+	guard let lastChar = word2.last, let firstChar = word.first, lastChar == firstChar else {
+	  self.alertTitle = "단어의 첫 글자가 이전 단어의 마지막 글자와 일치하지 않습니다."
+	  self.showAlert = true
+	  self.userInput = ""
+	  return
+	}
+	
+	// 유효성 검사
+	fetchWordInfo(word: word) { isValid in
+	  if isValid {
+		DispatchQueue.main.async {
+		  self.usedWords.insert(word)
+		  self.currentWord = word
+		  self.score += 1
+		  self.userInput = ""
+		}
+	  } else {
+		DispatchQueue.main.async {
+		  self.alertTitle = "잘못된 단어입니다"
+		  self.showAlert = true
+		}
+	  }
+	}
+	
+	self.userInput = "" // 입력 초기화
+	startTimer()
   }
-}
-
-self.userInput = "" // 입력 초기화
-
-}
-// 다크모드 전환
-func darkModeSwitch() {
-isDarkMode.toggle()
-}
-
-// 타이머 기능
-func startTimer() {
-timerIsActive = true
-timeRemaining = 5.0
-
-gameTimer?.invalidate() // 기존 타이머가 있다면 취소
-gameTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
-  DispatchQueue.main.async {
-	guard let self = self else { return }
-	if self.timeRemaining > 0.1 {
-	  self.timeRemaining -= 0.1
-	} else {
-	  self.gameTimer?.invalidate()
+  // 다크모드 전환
+  func darkModeSwitch() {
+	isDarkMode.toggle()
+  }
+  
+  // 타이머 기능
+  func startTimer() {
+	timerIsActive = true
+	timeRemaining = 10.0
+	
+	gameTimer?.invalidate() // 기존 타이머가 있다면 취소
+	gameTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+	  DispatchQueue.main.async {
+		guard let self = self else { return }
+		if self.timeRemaining > 0.1 {
+		  self.timeRemaining -= 0.1
+		} else {
+		  self.gameTimer?.invalidate()
+		  self.timerIsActive = false
+		  self.timerExpired()
+		}
+	  }
+	}
+	
+  }
+  
+  // 타이머 제한시간 만료, 타이머 재시작
+  func timerExpired() {
+	DispatchQueue.main.async {
+	  self.alertTitle = "제한 시간 초과"
+	  self.showAlert = true
+	  // 타이머를 정지합니다.
 	  self.timerIsActive = false
-	  self.timerExpired()
 	}
   }
-}
-
-}
-
-// 타이머 제한시간 만료, 타이머 재시작
-func timerExpired() {
-DispatchQueue.main.async {
-self.alertTitle = "제한 시간 초과"
-self.showAlert = true
-// 타이머를 정지합니다.
-self.timerIsActive = false
-}
-}
-
-// 북마크 기능
-func bookmarkCurrentWord() {
-bookmarkedWords.insert(currentWord)
-}
-// 영구저장
-func saveBookmarks() {
-UserDefaults.standard.set(Array(bookmarkedWords), forKey: "BookmarkedWords")
-}
-// 북마크 불러오기
-func loadBookmarks() {
-if let savedWords = UserDefaults.standard.array(forKey: "BookmarkedWords") as? [String] {
-bookmarkedWords = Set(savedWords)
-if bookmarkedWords.isEmpty {
-self.alertTitle = "북마크가 없습니다."
-self.showAlert = true
-} else {
-self.alertTitle = "북마크가 없습니다"
-self.showAlert = true
-}
-}
-}
-// 게임 초기화
-func resetGame() {
-self.currentWord = wordManager.pickRandomWord()
-usedWords.removeAll()
-score = 0
-userInput = ""
-startTimer()
-}
+  
+  // 북마크 기능
+  func bookmarkCurrentWord() {
+	bookmarkedWords.insert(currentWord)
+  }
+  // 영구저장
+  func saveBookmarks() {
+	UserDefaults.standard.set(Array(bookmarkedWords), forKey: "BookmarkedWords")
+  }
+  // 북마크 불러오기
+  func loadBookmarks() {
+	if let savedWords = UserDefaults.standard.array(forKey: "BookmarkedWords") as? [String] {
+	  bookmarkedWords = Set(savedWords)
+	  if bookmarkedWords.isEmpty {
+		self.alertTitle = "북마크가 없습니다."
+		self.showAlert = true
+	  } else {
+		self.alertTitle = "북마크가 없습니다"
+		self.showAlert = true
+	  }
+	}
+  }
+  // 게임 초기화
+  func resetGame() {
+	self.currentWord = wordManager.pickRandomWord()
+	usedWords.removeAll()
+	score = 0
+	userInput = ""
+	startTimer()
+  }
 }
